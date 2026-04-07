19c2bcdaba2c79c9cc2e028f17ac99d7515b8059
cesarledres <cesarledrescv@gmail.com>
2026-04-06 21:29:01 -0300

    ajuste no exercicio 5

--- estrutura-repeticao/exercicio5.py
+++ estrutura-repeticao/exercicio5.py
@@ -4,5 +4,8 @@ Ler um valor N e imprimir todos os valores inteiros entre 1 (inclusive) e N (inc
 
 n = int(input("Digite um número para exibir do 1 até este: "))
 
-for cont in range(1, n+1):
-    print(cont)
+if n > 0:
+    for cont in range(1, n+1):
+        print(cont)
+else:
+    print("O valor de N deve ser sempre maior que 0.")
